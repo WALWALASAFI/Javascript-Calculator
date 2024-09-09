e7aaf9b9da86ac37589c74bd840d05cc0422d64b
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-09 09:53:53 +0430

    import calculator in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Calculator from './Calculator';
+import './index.css';
 
-function App() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <Calculator />
-        </div>
-    );
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
 
 export default App;
