12190b1edc3942b8d802654ba6e264cb3b9a2c60
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-10 10:01:17 +0430

    correct the missing trailing comma

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root') // No trailing comma needed here
+  document.getElementById('root') // eslint-disable-line comma-dangle
 );
